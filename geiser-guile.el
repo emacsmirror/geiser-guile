c641fcc50b6b86ca95743122b5206cdcd475f96e
jao <jao@gnu.org>
2022-03-23 23:51:52 +0000

    Version bump to geiser 0.23.2
    Fixes issue #18.

--- geiser-guile.el
+++ geiser-guile.el
@@ -7,9 +7,9 @@
 ;; Maintainer: Jose Antonio Ortega Ruiz (jao@gnu.org)
 ;; Keywords: languages, guile, scheme, geiser
 ;; Homepage: https://gitlab.com/emacs-geiser/guile
-;; Package-Requires: ((emacs "25.1") (geiser "0.23.1"))
+;; Package-Requires: ((emacs "25.1") (geiser "0.23.2"))
 ;; SPDX-License-Identifier: BSD-3-Clause
-;; Version: 0.23
+;; Version: 0.23.2
 
 ;; This file is NOT part of GNU Emacs.
 
