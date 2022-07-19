7a4945fbc199ec823da1be7283f6e81f83b2609b
jao <jao@gnu.org>
2022-07-19 02:43:37 +0100

    Equivalent fix to the previous fix that reflects original intent
    program-arities is expected (in its only usage a few lines below its
    definition) to return #f if program-address-range doesn't return anything
    useful, and i was incorrectly using when instead of and for that.

--- src/geiser/doc.scm
+++ src/geiser/doc.scm
@@ -78,9 +78,7 @@
 
 (define (program-arities prog)
   (let ((addrs (program-address-range prog)))
-    (if (pair? addrs)
-        (find-program-arities (car addrs))
-        '())))
+    (and (pair? addrs) (find-program-arities (car addrs)))))
 
 (define (arguments proc)
   (define (p-args prog)
